2f63adba30dd0f71400e52f959f38b67e07816ca
Binh Tran <binhtran432k@gmail.com>
2025-01-23 13:49:02 +0700

    refactor(translate): use translateDiff instead

--- src/translate.ts
+++ src/translate.ts
@@ -1,6 +1,6 @@
 import { SvgPathSizeMap, type SvgPaths } from "./svg-paths.js";
 
-export function translate(
+export function translateDiff(
 	svgPaths: SvgPaths,
 	dx: number,
 	dy: number,
@@ -8,29 +8,39 @@ export function translate(
 	let walkIdx = 0;
 	for (const cmd of svgPaths.cmds) {
 		const upperCmd = cmd.toUpperCase();
+		const isAbsolute = cmd === upperCmd;
 
 		// biome-ignore lint/style/noNonNullAssertion:
 		const size = SvgPathSizeMap.get(upperCmd)!;
-
-		const idx = walkIdx;
-		walkIdx += size;
-
-		const isAbsolute = cmd === upperCmd;
-		if (!isAbsolute) continue;
-
-		if (upperCmd === "H") {
-			svgPaths.values[idx] += dx;
-		} else if (upperCmd === "V") {
-			svgPaths.values[idx] += dy;
-		} else if (upperCmd === "A") {
-			svgPaths.values[idx + 5] += dx;
-			svgPaths.values[idx + 6] += dy;
+		if (isAbsolute) {
+			walkIdx = translateCmdDiff(svgPaths, upperCmd, size, walkIdx, dx, dy);
 		} else {
-			for (let i = 0; i < size; i += 2) {
-				svgPaths.values[idx + i] += dx;
-				svgPaths.values[idx + i + 1] += dy;
-			}
+			walkIdx += size;
 		}
 	}
 	return svgPaths;
+}
+
+export function translateCmdDiff(
+	svgPaths: SvgPaths,
+	upperCmd: string,
+	size: number,
+	idx: number,
+	dx: number,
+	dy: number,
+): number {
+	if (upperCmd === "H") {
+		svgPaths.values[idx] += dx;
+	} else if (upperCmd === "V") {
+		svgPaths.values[idx] += dy;
+	} else if (upperCmd === "A") {
+		svgPaths.values[idx + 5] += dx;
+		svgPaths.values[idx + 6] += dy;
+	} else {
+		for (let i = 0; i < size; i += 2) {
+			svgPaths.values[idx + i] += dx;
+			svgPaths.values[idx + i + 1] += dy;
+		}
+	}
+	return idx + size;
 }
