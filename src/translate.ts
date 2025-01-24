de70c19c2cb8573f908903e9482559676f6167eb
Binh Tran <binhtran432k@gmail.com>
2025-01-24 11:57:08 +0700

    refactor(translate): migrate to switch from if/else

--- src/translate.ts
+++ src/translate.ts
@@ -4,17 +4,23 @@ export function translateDiff(
 	dx: number,
 	dy: number,
 ): void {
-	if (upperCmd === "H") {
-		values[0] += dx;
-	} else if (upperCmd === "V") {
-		values[0] += dy;
-	} else if (upperCmd === "A") {
-		values[5] += dx;
-		values[6] += dy;
-	} else if (upperCmd !== "Z") {
-		for (let i = 0; i < values.length; ) {
-			values[i++] += dx;
-			values[i++] += dy;
-		}
+	switch (upperCmd) {
+		case "H":
+			values[0] += dx;
+			break;
+		case "V":
+			values[0] += dy;
+			break;
+		case "A":
+			values[5] += dx;
+			values[6] += dy;
+			break;
+		case "Z":
+			break;
+		default:
+			for (let i = 0; i < values.length; ) {
+				values[i++] += dx;
+				values[i++] += dy;
+			}
 	}
 }
